cd93bd55ac71b8c94d1b026c2898b6b32cc96c79
Sara1brahim <20912023200685@fci.zu.edu.eg>
2026-02-16 20:13:01 +0200

    Add SQL queries

--- MySQL/Task_1/Task_2_Solutions.sql
+++ MySQL/Task_1/Task_2_Solutions.sql
@@ -1,1 +1,26 @@
+-- 1) Recyclable and Low Fat Products
+
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y'
+  AND recyclable = 'Y';
+
+
+
+-- 2) Big Countries
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000
+   OR population >= 25000000;
+
+
+
+-- 3) Find Customer Referee
+
+
+SELECT name
+FROM Customer
+WHERE referee_id <> 2
+   OR referee_id IS NULL;
 
